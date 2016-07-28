8fed5c0327170edc70b6f8d23c6ea1cd4ae95ce9
Solbiati Alessandro <alexsolbiati@hotmail.it>
2016-07-28 14:28:40 +0200

    Update main.R

--- R-Code/main.R
+++ R-Code/main.R
@@ -1,5 +1,5 @@
 #
-#	Alessandro Solbiati - EWMA_RiskMetrics GITHUB project
+#	Alessandro Solbiati - EWMA_RiskMetrics GITHUB project - 26/06/2016 
 #	reference: Quantitative Finance for R (Bee, Santi 2013)
 #	reference: RiskMetrics(TM) Technical Document (JPMorgan and Retuters 1996) 
 #
@@ -7,7 +7,7 @@
 #
 #	EWMA_RiskMetrics main function: computes and test all the process of the EWMA Volatility and VaR estimation
 #
-#	functions required:
+#	functions required: (in the same folder)
 #
 #	calc_Volatility()			[calc_Volatility.R]
 #	calc_VaRnp()				[calc_VaRnp.R     ]
@@ -15,6 +15,9 @@
 #	calc_VaRt()					[calc_VaRt.R      ]
 #	exe_backtesting()			[exe_backtesting.R]
 #
+# The function return an matrix 'EWMA_result'
+# To obtain the p-value of the binomial test on the effectiviness of the computed Value at Risk use "as.numeric(EWMA_RiskMetrics(...)[1,5])"
+# For further informations see the documentations on the project page
 
 
 EWMA_RiskMetrics<- function(Serie,conf,usage,s_startdate,s_enddate)
@@ -39,6 +42,8 @@ EWMA_RiskMetrics<- function(Serie,conf,usage,s_startdate,s_enddate)
   if (usage==1) { VaR_Z<- calc_VaRnp(-Z_t,conf,VaR=TRUE,s_startdate=s_startdate,s_enddate=s_enddate) }
   if (usage==2) { VaR_Z<- calc_VaRnorm(-Z_t,conf,VaR=TRUE,s_startdate=s_startdate,s_enddate=s_enddate) }
   if (usage==3) { VaR_Z<- calc_VaRt(-Z_t,conf,VaR=TRUE,s_startdate=s_startdate,s_enddate=s_enddate) }
+  #in the 3rd VaR_Z method (student's t) there could be a convergence error due to the initial value of the computing algorithm, 
+  #to fix this issue just need to add the argument "init=c(a,b,c)" where a, b and c are three numbers you can aribtrarily choose
   
   
   #Compute conditioned VaR serie
@@ -53,6 +58,6 @@ EWMA_RiskMetrics<- function(Serie,conf,usage,s_startdate,s_enddate)
   EWMA_result=cbind(SSt,test$p.value,test$estimate)
   EWMA_result
   
-  #to get the test$p.value use the command "as.numeric(EWMA_result[1,5])"
+  #to get the test$p.value use the command "as.numeric(EWMA_RiskMetrics(...)[1,5])"
   
 }
