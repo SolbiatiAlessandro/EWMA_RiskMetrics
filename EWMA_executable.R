a85ffa43bd8b8413d8c95fc162a4bd63a83e3d5f
Solbiati Alessandro <alexsolbiati@hotmail.it>
2016-07-31 16:18:15 +0200

    Update EWMA_executable.R

--- EWMA_executable.R
+++ EWMA_executable.R
@@ -3,28 +3,10 @@
 #	reference: Quantitative Finance for R (Bee, Santi 2013)
 #	reference: RiskMetrics(TM) Technical Document (JPMorgan and Retuters 1996) 
 #
-#	--------------------------------------------------------------------------
+#	------------------------------------------------------------------------------------------------------
 #	|||				EXECUTABLE FILE (with all functions needed)			   |||
-#	--------------------------------------------------------------------------
+#	------------------------------------------------------------------------------------------------------
 
-#	EWMA_RiskMetrics() main function: computes and tests all the processes of the EWMA Volatility and VaR estimations
-#
-#	USAGE:
-#	- load the file on the R console
-#	- just create an object 
-#	"my_object <- EWMA_RiskMetrics(Serie, conf, usage, s_startdate, s_enddate)" [see documentations for help]
-#   - The function return a matrix 'EWMA_result'
-#   - To obtain the p-value of the binomial test on the effectiviness of the computed Value at Risk use "as.numeric(my_object[1,5])"
-#   
-#	For further informations see the documentations on the project page
-#
-#	functions required: 
-#
-#	calc_Volatility()			[calc_Volatility.R]
-#	calc_VaRnp()				[calc_VaRnp.R     ]
-#	calc_VaRnorm()				[calc_VaRnorm.R   ]
-#	calc_VaRt()					[calc_VaRt.R      ]
-#	exe_backtesting()			[exe_backtesting.R]
 
 
 calc_VaRnorm<- function(Serie,conf,VaR,s_startdate,s_enddate){
@@ -158,4 +140,107 @@ EWMA_RiskMetrics<- function(Serie,conf,usage,s_startdate,s_enddate){
   
   #to get the test$p.value use the command "as.numeric(EWMA_RiskMetrics(...)[1,5])"
   
+}
+
+getprice <- function(name_code){
+	
+	require(tseries)
+	require(timeSeries)
+		
+	
+	pp <- get.hist.quote(instrument=name_code,quote="Close")
+	pp <- as.timeSeries(pp)
+	pp
+	
+
+}
+
+
+pvs_mat <- function(serie,start,end){
+
+	pvs <- matrix(nrow=3,ncol=4)
+	rownames(pvs) <- c("Non-Parametric","Normal Distr","Student T Distr")
+	colnames(pvs) <- c("conf 0.90","conf 0.95","conf 0.99","conf 0.995")
+
+	conf <- 0.9
+	j <- 1
+	for(i in 1:3){
+		pvs[i,j]=as.numeric(EWMA_RiskMetrics(serie,conf,i,start,end,VaR=TRUE)[1,5])
+		}
+
+	conf <- 0.95
+	j <- 2
+	for(i in 1:3){
+		pvs[i,j]=as.numeric(EWMA_RiskMetrics(serie,conf,i,start,end,VaR=TRUE)[1,5])
+		}
+		
+	conf <- 0.99
+	j <- 3
+	for(i in 1:3){
+		pvs[i,j]=as.numeric(EWMA_RiskMetrics(serie,conf,i,start,end,VaR=TRUE)[1,5])
+		}
+
+	conf <- 0.995
+	j <- 4
+	for(i in 1:3){
+		pvs[i,j]=as.numeric(EWMA_RiskMetrics(serie,conf,i,start,end,VaR=TRUE)[1,5])
+	}
+	
+	pvs
+	}
+
+
+max_pv <- function(serie,start,end){
+	
+	my_mat <- pvs_mat(serie,start,end)
+	
+	pv <- 0
+	row <- 0
+	col <- 0
+	for(i in 1:3){
+		for(j in 1:4){
+			if(my_mat[i,j]>pv){
+				pv <- my_mat[i,j]
+				row=i
+				col=j
+			}
+		}
+	}
+	
+	out <- c(pv,row,col)
+	out
+	
+}
+
+study <- function(serie,start,end){
+	
+	vect <- max_pv(serie,start,end)
+	usage <- vect[2]
+	col <- vect[3]
+	
+	if(col==1){conf <- 0.9}
+	if(col==2){conf <- 0.95}
+	if(col==3){conf <- 0.99}
+	if(col==4){conf <- 0.995}
+	
+	ewma_VaR <- EWMA_RiskMetrics(serie, conf, usage, start, end, VaR=TRUE)
+	ewma_ES <- EWMA_RiskMetrics(serie, conf, usage, start, end, VaR=FALSE)
+	l=length(ewma_VaR[,1])
+	
+	out <- cbind("Value at Risk"=ewma_VaR[,4],"Expected Shortfall"=ewma_ES[,4],"p-value"=ewma_VaR[l,5])
+	out <- cbind(out[l,],"confidence"=conf)
+	out
+	
+}
+
+
+display_study <- function(titles, start, end){
+
+	studies_var <- c()
+	for(i in 1:length(titles)){
+		sst <- getprice(titles[i])
+		studies_var[i]=study(sst,"2000-01-01","2015-12-31")[1]
+	}
+
+	studies_var
 }
