9eaf8c2c28ac0fbda6811d3e819108c99c35ab59
Solbiati Alessandro <alexsolbiati@hotmail.it>
2016-08-04 18:16:15 +0200

    Update display_study.R

--- utility/display_study.R
+++ utility/display_study.R
@@ -9,12 +9,12 @@
 #	display_study() : return an array of VaR from the different titles in input (this is the main utility funct)
 #
 
-display_study <- function(titles, start, end){
+display_study <- function(titles, sl, bl){
 
 	studies_var <- c()
 	for(i in 1:length(titles)){
 		sst <- getprice(titles[i])
-		studies_var[i]=study(sst,start,end)[1]
+		studies_var[i]=study(sst,length(sst)-sl-bl,length(sst)-bl)[1]
 	}
 
 	studies_var
